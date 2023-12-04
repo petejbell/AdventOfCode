ae0f3170d9911eb22589ca433f8b9245d4b84222
Pete Bell <stuff@petebell.com>
2023-12-04 00:46:16 +0000

    Day 3.1

--- 2023/Day03/Day03.1 commented.py
+++ 2023/Day03/Day03.1 commented.py
@@ -1,16 +1,15 @@
 allow = False
 sum_part_numbers = []
 
-
 def all_symbols():
     with open('2023/Day03/input.txt', 'r') as f:
         symbols = ''
         for line in f:
             for chr in line.strip():
                 if not (chr.isdigit() or chr == '.'):
-                    if chr not in symbols:
-                        symbols += chr
-        return (symbols)
+                        if chr not in symbols:
+                            symbols += chr
+        return(symbols)
 
 
 with open('2023/Day03/input.txt', 'r') as f:
@@ -18,6 +17,7 @@ with open('2023/Day03/input.txt', 'r') as f:
     print(symbols)
     lines = f.readlines()
     for i in range(0, len(lines)):
+        #print('LINE NUMBER',i+1)
         line = lines[i]
         if i < len(lines)-1:
             nextline = lines[i+1]
@@ -26,17 +26,20 @@ with open('2023/Day03/input.txt', 'r') as f:
         part_number = ''
         for index, item in enumerate(line):
             if item.isdigit():
-                part_number += item
+                part_number += item             
             elif len(part_number) > 0:
+                #print(part_number) #Put check for symbols instead of this line
+                
                 # Check to the right
                 if item in symbols:
                     allow = True
-
+                
                 # Check to the left
                 if line[index-len(part_number)-1] in symbols:
                     allow = True
-
+                
                 # Check for symbols below
+                #print(nextline[index])
                 if nextline[index] in symbols:
                     allow = True
                 for digit in range(len(part_number)+1):
@@ -55,5 +58,17 @@ with open('2023/Day03/input.txt', 'r') as f:
                 allow = False
 sum = 0
 for valid in sum_part_numbers:
-    sum += int(valid)
+    sum += int(valid)        
+#print(sum_part_numbers)        
 print(sum)
+    
+    
+
+    
+    
+
+
+
+
+
+
